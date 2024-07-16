6b44d2da051adc797d526b2289b88df305490f17
audd <audd@intern>
2024-07-16 17:22:37 +0800

    Batch File to run both files together

---
+++ json_in_msg_file/batch script (both msg_extraction and extracted_processing in same directory)/qns_extract_process_classify.bat
@@ -0,0 +1,40 @@
+echo off
+set raw_file_folder=%~1
+set extracted_file_folder=%~2
+
+::echo raw_file_folder=%raw_file_folder%
+::echo extracted_file_folder=%extracted_file_folder%
+
+if not "%raw_file_folder%" == "" (
+    IF not "%extracted_file_folder%" == "" (
+        ::echo 1
+        python qns_extraction.py -r "%raw_file_folder%" -e "%extracted_file_folder%"
+        ::python -c "print()"
+        ::python -c "print('Middle')"
+        ::python -c "print()"
+        python extracted_processing.py -e "%extracted_file_folder%"
+    ) ELSE (
+        ::echo 2
+        python qns_extraction.py -r "%raw_file_folder%"
+        ::python -c "print()"
+        ::python -c "print('Middle')"
+        ::python -c "print()"
+        python extracted_processing.py
+    )
+) ELSE (
+    if not "%extracted_file_folder%" == "" (
+        ::echo 3
+        python qns_extraction.py -e "%extracted_file_folder%"
+        ::python -c "print()"
+        ::python -c "print('Middle')"
+        ::python -c "print()"
+        python extracted_processing.py -e "%extracted_file_folder%"
+    ) ELSE (
+        ::echo 4
+        python qns_extraction.py
+        ::python -c "print()"
+        ::python -c "print('Middle')"
+        ::python -c "print()"
+        python extracted_processing.py
+    )
+)
